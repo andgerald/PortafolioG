29e00e96b208afb132ee1019fe863c5117602f8c
andgerald <geraldinebecerra93@gmail.com>
2024-07-23 17:10:59 -0400

    se agregan rutas  para proyectos, sobre mi y contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'sobreMi', component: SobreMiComponent },
+  { path: 'contacto', component: ContactoComponent },
+
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
