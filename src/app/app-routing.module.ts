eb9fc82bbef2ff15a3d2dc2975a57db242003666
andgerald <geraldinebecerra93@gmail.com>
2024-07-24 00:27:47 -0400

    se agrega la tura incio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
 
   { path: 'proyectos', component: ProyectosComponent },
   { path: 'sobreMi', component: SobreMiComponent },
   { path: 'contacto', component: ContactoComponent },
+  { path: 'inicio', component: InicioComponent },
+
 
 
   
